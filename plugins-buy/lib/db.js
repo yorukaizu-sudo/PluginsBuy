1ee73a74cf196be8391c2cbd093257635b44b798
yorukaizu-sudo <yorukaizu@gmail.com>
2026-09-10 06:47:02 +0700

    Upload via GitHub Uploader Pro - add plugins-buy/lib/db.js

---
+++ plugins-buy/lib/db.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { 
+        conn: null, 
+        promise: null 
+    };
+}
+
+export async function connectDB() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+        cached.promise = mongoose.connect(
+            process.env.MONGODB_URI, 
+            opts
+        );
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
+}
